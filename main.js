bcc529d94498c64427a8abff287cd35377bdf1f0
Ryan Valera <valeraryan@gmail.com>
2022-09-17 18:04:04 -0700

    Modified HTML fields for testing and added form template. Styled CSS for the form template. Began adding starting code for JS

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-let myLibrary = [];
+const book1 = new Book('Hobbit', 'Tolkien', 'Pages', true, 5);
+let myLibrary = [book1];
+let bookID = 20220
+const addBook = document.querySelector('.add-book');
+addBook.addEventListener('click', addBookToLibrary);
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, bookID) {
+    this.id = bookID;
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = (read) => {
-        if (read) {
-            return "Read";
-        } else {
-            return "Not Read";
-        }
-    }
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read()}`;
-    }
+    this.read = read;
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(e) {
+    e.preventDefault();
+    console.log(myLibrary);
+    const inputs = document.querySelectorAll('input');
+    const status = document.querySelector('.status');
+    const title = inputs[1].value;
+    const author = inputs[2].value;
+    const pages = inputs[3].value;
+    const read = status.value;
+    myLibrary.push(new Book(title, author, pages, read, bookID));
+    bookID++;
+    addTile();
 }
 
 
-// const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
-// console.log(theHobbit.info());
+
+
+
