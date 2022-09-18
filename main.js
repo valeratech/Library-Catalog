b700d3058b58000e92bb20120c8bc4ef19a4ff65
Ryan Valera <valeraryan@gmail.com>
2022-09-18 12:51:00 -0700

    Added input button for read/unread on individual tiles. Adjusted to JS file to correctly pull value from textarea field

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const book1 = new Book('Hobbit', 'Tolkien', 'Pages', true, 5);
 let myLibrary = [book1];
 let bookID = 20220
-const addBook = document.querySelector('.add-book');
-addBook.addEventListener('click', addBookToLibrary);
 
 function Book(title, author, pages, date, summary, read, bookID) {
-    this.id = bookID;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.date = date;
     this.summary = summary;
     this.read = read;
+    this.id = bookID;
 }
 
 function addBookToLibrary(e) {
@@ -19,12 +17,15 @@ function addBookToLibrary(e) {
     console.log(myLibrary);
     const inputs = document.querySelectorAll('input');
     const status = document.querySelector('.status');
-    const title = inputs[1].value;
-    const author = inputs[2].value;
-    const pages = inputs[3].value;
-    const date = inputs[4].value;
+    const textArea = document.querySelector('textarea')
+    const title = inputs[2].value;
+    const author = inputs[3].value;
+    const pages = inputs[4].value;
+    const date = inputs[5].value;
+    const summary = textArea.value;
     const read = status.value;
-    myLibrary.push(new Book(title, author, pages, date, read, bookID));
+    console.log(inputs);
+    myLibrary.push(new Book(title, author, pages, date, summary, read, bookID));
     bookID++;
     addTile();
 }
@@ -36,7 +37,8 @@ function addTile() {
     const myTitle = document.createElement('h3');
     const myAuthor = document.createElement('p');
     const myPages = document.createElement('p');
-    const myDate = document.createElement('p')
+    const myDate = document.createElement('p');
+    const myUl = document.createElement('ul');
 
     myTile.className = 'display-tile';
     myTitle.className = 'tile-heading';
@@ -44,19 +46,47 @@ function addTile() {
     myPages.className = 'tile-field';
     myDate.className = 'tile-field';
 
+
+
+
     myLibrary.forEach((book) => {
         console.log(book);
         console.log(book.title);
 
         myTitle.textContent = book.title;
-        myAuthor.textContent = `Author:  ${book.author}`
-        myPages.textContent = `Pages:  ${book.pages}`
-        myDate.textContent = `Date:  ${book.date}`
+        myAuthor.textContent = `Author:  ${book.author}`;
+        myPages.textContent = `Pages:  ${book.pages}`;
+        myDate.textContent = `Date:  ${book.date}`;
+        myUl.innerHTML = `<li><input type="checkbox" name="power" id="power">
+                          <label for="power"><span class="on">Read</span><span class="off">Unread</span></label></li>`;
 
         myTile.appendChild(myTitle);
         myTile.appendChild(myAuthor);
         myTile.appendChild(myPages);
         myTile.appendChild(myDate);
+        myTile.appendChild(myUl);
         myArticle.appendChild(myTile);
+
+        const mySummary = document.createElement('p');
+        mySummary.textContent = `${book.summary}`
+        mySummary.textContent = `${book.summary}`
+        mySummary.className = 'tile-field';
+
+        // myTile.appendChild(mySummary); // Will specify summary info on the back of tile/card
     });
 }
+
+const addBook = document.querySelector('.add-book');
+addBook.addEventListener('click', addBookToLibrary);
+
+// const toggleRead = document.querySelectorAll('input[type="checkbox"]');
+const displayTiles = document.querySelector('.display-tiles');
+displayTiles.addEventListener('change', (e) => {
+    console.log(e.target.checked);
+    /* RETREIVES THE INDEX OF THE SPECIFIC TILES WHERE THE BUTTON IS TOGGLED */
+    const parent = e.target.parentNode.parentNode.parentNode.parentNode;
+    const child = e.target.parentNode.parentNode.parentNode;
+    const index = Array.prototype.indexOf.call(parent.children, child);
+    console.log(index);
+    console.log(myLibrary);
+});
