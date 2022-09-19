14a32b34bfe967b0a70849c7c00bf16d323f26f6
Ryan Valera <valeraryan@gmail.com>
2022-09-18 18:09:46 -0700

    Changed the headerjpg. Added JS functionality of the read and unread button for the book tiles. Added 'Date Entered' field for each book tile

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const book1 = new Book('Hobbit', 'Tolkien', 'Pages', true, 5);
-let myLibrary = [book1];
+// const book1 = new Book('Hobbit', 'Tolkien', 'Pages', true, 5);
+let myLibrary = [];
 let bookID = 20220
 
 function Book(title, author, pages, date, summary, read, bookID) {
@@ -14,56 +14,70 @@ function Book(title, author, pages, date, summary, read, bookID) {
 
 function addBookToLibrary(e) {
     e.preventDefault();
-    console.log(myLibrary);
     const inputs = document.querySelectorAll('input');
     const status = document.querySelector('.status');
     const textArea = document.querySelector('textarea')
-    const title = inputs[2].value;
-    const author = inputs[3].value;
-    const pages = inputs[4].value;
-    const date = inputs[5].value;
-    const summary = textArea.value;
-    const read = status.value;
     console.log(inputs);
+    const title = inputs[1].value;
+    const author = inputs[2].value;
+    const pages = inputs[3].value;
+    const date = inputs[4].value;
+    const summary = textArea.value;
+    const read = (status.value === 'true'); // convert value to a boolean
+
     myLibrary.push(new Book(title, author, pages, date, summary, read, bookID));
     bookID++;
-    addTile();
+    createTile(read);
 }
 
-function addTile() {
+
+function createTile(read) {
     const myArticle = document.querySelector('.display-tiles');
 
     const myTile = document.createElement('div');
     const myTitle = document.createElement('h3');
     const myAuthor = document.createElement('p');
     const myPages = document.createElement('p');
-    const myDate = document.createElement('p');
+    const myPubDate = document.createElement('p');
+    const myInsertDate = document.createElement('p');
     const myUl = document.createElement('ul');
+    const insertDate = new Date();
 
     myTile.className = 'display-tile';
     myTitle.className = 'tile-heading';
     myAuthor.className = 'tile-field';
     myPages.className = 'tile-field';
-    myDate.className = 'tile-field';
-
-
-
+    myPubDate.className = 'tile-field';
+    myInsertDate.className = 'tile-field';
 
     myLibrary.forEach((book) => {
-        console.log(book);
-        console.log(book.title);
-
+        // console.log(book);
+        // console.log(book.title);
         myTitle.textContent = book.title;
         myAuthor.textContent = `Author:  ${book.author}`;
         myPages.textContent = `Pages:  ${book.pages}`;
-        myDate.textContent = `Date:  ${book.date}`;
-        myUl.innerHTML = `<li><input type="checkbox" name="power" id="power">
+        myPubDate.textContent = `Date Published:  ${book.date}`;
+        myInsertDate.textContent = `Date Entered: ${insertDate.getFullYear()}-${insertDate.getMonth()}-${insertDate.getDate()}`
+
+        if (read) {
+            myUl.innerHTML = `<li><input type="checkbox" name="power" id="power" checked>
                           <label for="power"><span class="on">Read</span><span class="off">Unread</span></label></li>`;
+            myTile.style.backgroundImage = 'linear-gradient(to right,  #406840, #a6dfa6)';
+            // const myStatus = document.querySelector('input[type="checkbox"]');
+            // console.log(myStatus);
+        } else {
+            myUl.innerHTML = `<li><input type="checkbox" name="power" id="power">
+                <label for="power"><span class="on">Read</span><span class="off">Unread</span></label></li>`;
+            myTile.style.backgroundImage = 'linear-gradient(to right, #bababa, #969696)';
+            // const myStatus = document.getElementById("checkbox");
+            // console.log(myStatus)
+        }
 
         myTile.appendChild(myTitle);
         myTile.appendChild(myAuthor);
         myTile.appendChild(myPages);
-        myTile.appendChild(myDate);
+        myTile.appendChild(myPubDate);
+        myTile.appendChild(myInsertDate);
         myTile.appendChild(myUl);
         myArticle.appendChild(myTile);
 
@@ -82,11 +96,14 @@ addBook.addEventListener('click', addBookToLibrary);
 // const toggleRead = document.querySelectorAll('input[type="checkbox"]');
 const displayTiles = document.querySelector('.display-tiles');
 displayTiles.addEventListener('change', (e) => {
-    console.log(e.target.checked);
     /* RETREIVES THE INDEX OF THE SPECIFIC TILES WHERE THE BUTTON IS TOGGLED */
     const parent = e.target.parentNode.parentNode.parentNode.parentNode;
     const child = e.target.parentNode.parentNode.parentNode;
     const index = Array.prototype.indexOf.call(parent.children, child);
-    console.log(index);
-    console.log(myLibrary);
+    /* It has to be background-image, not background-color, for gradients. */
+    if (e.target.checked) {
+        child.style.backgroundImage = 'linear-gradient(to right,  #406840, #a6dfa6)';
+    } else {
+        child.style.backgroundImage = 'linear-gradient(to right, #bababa, #969696)';
+    }
 });
