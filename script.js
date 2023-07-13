569cd2e8013e3f83f1d3726895847df332e04796
Ryan Valera <valeraryan@gmail.com>
2023-07-13 02:18:55 -0700

    Created an additional script.js file with optomize code. For test purposes this was ran through chatgpt

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function createBook (title, author, pages, date, summary, read, bookID) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.date = date;
+        this.summary = summary;
+        this.read = read;
+        this.id = bookID;
+    }
+
 const book1 = new createBook('The Art of War', 'Sun Tzu', 256, null, ` The Art Of War has been 
 considered the definitive text on military strategy and warfare ever since being written in ancient China around 500 BC, 
 inspiring businesses, athletes, and of course generals to beat their opponents and competition the right way until 
@@ -16,13 +26,13 @@ education, and marriage in the society of the landed gentry of the British Regen
 daughters of a country gentleman, Mr. Bennet, living in Longbourn.`, false, 3);
 
 const book4 = new createBook('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 223,
-'1997-06-26', `Harry Potter has no idea how famous he is. That's because he's being raised by his 
+    '1997-06-26', `Harry Potter has no idea how famous he is. That's because he's being raised by his 
 miserable aunt and uncle who are terrified Harry will learn that he's really a wizard, just as his parents were. 
 But everything changes when Harry is summoned to attend an infamous school for wizards, and he begins to discover 
 some clues about his illustrious birthright.`, false, 4);
 
-let myLibrary = [];
-let bookID = 20220
+const myLibrary = [];
+let bookID = 20220;
 
 myLibrary.push(book1);
 createTile(false);
@@ -33,164 +43,105 @@ createTile(false);
 myLibrary.push(book4);
 createTile(false);
 
-bookCount();
-// console.log(myLibrary)
-
-function createBook(title, author, pages, date, summary, read, bookID) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.date = date;
-    this.summary = summary;
-    this.read = read;
-    this.id = bookID;
-}
-
 function clearForm() {
     const form = document.querySelector('.book-form');
-    const inputs = form.querySelectorAll(':scope input')
+    const inputs = form.querySelectorAll(':scope input');
     const status = document.querySelector('.status');
-    const textArea = document.querySelector('textarea')
+    const textArea = document.querySelector('textarea');
 
-    inputs[0].value = "";
-    inputs[1].value = "";
-    inputs[2].value = "";
-    inputs[3].value = "";
-    textArea.value = "";
+    inputs.forEach((input) => (input.value = ''));
+    textArea.value = '';
 }
-
-const filter = document.querySelector('#filter');
-filter.addEventListener('keyup', filterBook);
 
 function filterBook(e) {
     const inputBox = filter.value.toLowerCase();
     const tiles = document.querySelectorAll('.display-tile-front');
-    const tileArray = Array.from(tiles);
-    tileArray.forEach(tile => {
+    tiles.forEach((tile) => {
         const title = tile.firstElementChild.textContent.toLowerCase();
         const author = tile.firstElementChild.nextElementSibling.textContent.toLowerCase();
-        // console.log(author);
-        title.indexOf(inputBox) != -1 || author.indexOf(inputBox) != -1 ? tile.parentElement.style.display = 'block'
-            : tile.parentElement.style.display = 'none';
-    })
-
+        tile.parentElement.style.display = title.includes(inputBox) || author.includes(inputBox) ? 'block' : 'none';
+    });
 }
 
-const readSelect = document.querySelector('#read');
-readSelect.addEventListener('change', sortRead);
-
 function sortRead() {
-    console.log(readSelect.value);
+    const value = readSelect.value;
     const tiles = document.querySelectorAll('.display-tile');
-    const tileArray = Array.from(tiles);
-    tileArray.forEach(tile => {
-        if (readSelect.value === 'read' && tile.style.backgroundImage.includes('58')) {
-            tile.style.display = 'none';
-        } else if (readSelect.value === 'unread' && tile.style.backgroundImage.includes('227')) {
+    tiles.forEach((tile) => {
+        const backgroundImage = tile.style.backgroundImage;
+        const isRead = backgroundImage.includes('58');
+        const isUnread = backgroundImage.includes('227');
+        if ((value === 'read' && isRead) || (value === 'unread' && isUnread)) {
             tile.style.display = 'none';
         } else {
             tile.style.display = 'block';
         }
-    })
+    });
 }
 
-
-const addBook = document.querySelector('#add-book');
-addBook.addEventListener('click', addBookToLibrary);
-
-// const formContainer = document.querySelector('.form-container');
-const bookForm = document.querySelector('.book-form');
-bookForm.addEventListener('mouseover', function (e) {
-    const submitFormButton = document.querySelector('#add-book');
-    const isValid = checkValidation();
-    if (isValid) {
-        submitFormButton.disabled = false;
-    } else {
-        submitFormButton.disabled = true;
-    }
-});
-
 function checkValidation() {
-    // Form validation below
-    //form
-    //inputs
     const formTitle = document.querySelector('.form-title');
     const formAuthor = document.querySelector('.form-author');
     const formPages = document.querySelector('.form-pages');
     const formDate = document.querySelector('.form-date');
     const addBook = document.querySelector('#add-book');
     const isValid = bookForm.checkValidity() && formAuthor.checkValidity() && formPages.checkValidity() && formDate.checkValidity();
-    isValid ? addBook.className = 'add-book-style' : addBook.className = 'add-book-disable';
+    addBook.className = isValid ? 'add-book-style' : 'add-book-disable';
     return isValid;
 }
 
-function addBookToLibrary(e) {;
+function addBookToLibrary(e) {
     e.preventDefault();
     const isValid = checkValidation();
     if (isValid) {
-        // The :scope pseudo-class restores the expected behavior, only matching selectors on descendants of the base element:
         const form = document.querySelector('.book-form');
-        const inputs = form.querySelectorAll(':scope input')
-
+        const inputs = form.querySelectorAll(':scope input');
         const status = document.querySelector('.status');
-        const textArea = document.querySelector('textarea')
+        const textArea = document.querySelector('textarea');
 
         const title = inputs[0].value;
         const author = inputs[1].value;
         const pages = inputs[2].value;
         const date = inputs[3].value;
         const summary = textArea.value;
-        const read = (status.value === 'true'); // convert value to a boolean
+        const read = status.value === 'true';
+        const newBook = new Book(title, author, pages, date, summary, read, bookID++);
 
-        myLibrary.push(new createBook(title, author, pages, date, summary, read, bookID));
-        bookID++;
+        myLibrary.push(newBook);
         createTile(read);
         addRemoveMsg();
         bookCount();
         clearForm();
-    // } else {
-    //     console.log('hi')
-    //     const formKey = document.querySelector('.form-key');
-    //     formKey.style.color = 'red';
     }
 }
 
 function addRemoveMsg() {
+    const mainDisplay = document.querySelector('.display');
     if (myLibrary.length === 0) {
-        const mainDisplay = document.querySelector('.display');
         const addCatalogMsg = document.createElement('div');
-        addCatalogMsg.className = 'catalog-message'
+        addCatalogMsg.className = 'catalog-message';
         addCatalogMsg.innerHTML = `<h2>There are no books in your catalog</h2>
-            <p><em>Select the add symbol in the bottom right corner to add books</em></p>`;
+      <p><em>Select the add symbol in the bottom right corner to add books</em></p>`;
         mainDisplay.appendChild(addCatalogMsg);
     } else if (myLibrary.length === 1 && document.querySelector('.catalog-message')) {
-        // condition checks if there is at least 1 tile and if catalog message already exists - then remove
         const catalogMsg = document.querySelector('.catalog-message');
         catalogMsg.remove();
     }
 }
 
 function bookCount() {
-    // Code not efficient on resources but for demo purposes
     const totalBooks = document.querySelectorAll('.book-log p');
     totalBooks[0].textContent = `Total Books: ${myLibrary.length}`;
     let readBook = 0;
     let unreadBook = 0;
-    for (book of myLibrary) {
-        if (book.read) {
-            readBook += 1;
-        } else {
-            unreadBook += 1;
-        }
-    }
+    myLibrary.forEach((book) => {
+        book.read ? readBook++ : unreadBook++;
+    });
     totalBooks[1].textContent = `Read: ${readBook}`;
     totalBooks[2].textContent = `Unread: ${unreadBook}`;
 }
 
-
 function createTile(read) {
     const myArticle = document.querySelector('.display-tiles');
-
     const myTile = document.createElement('div');
     const myTileFront = document.createElement('div');
     const myTileBack = document.createElement('div');
@@ -231,12 +182,11 @@ function createTile(read) {
 
         if (read) {
             myUl.innerHTML = `<li><input type="checkbox" name="power" id="power" checked>
-                          <label for="power"><span class="on">Read</span><span class="off">Unread</span></label></li>`;
+                        <label for="power"><span class="on">Read</span><span class="off">Unread</span></label></li>`;
             myTile.style.backgroundImage = 'linear-gradient(135deg, #3a5271 0%, #8bd8e2 100%)';
-
         } else {
             myUl.innerHTML = `<li><input type="checkbox" name="power" id="power">
-                <label for="power"><span class="on">Read</span><span class="off">Unread</span></label></li>`;
+          <label for="power"><span class="on">Read</span><span class="off">Unread</span></label></li>`;
             myTile.style.backgroundImage = 'linear-gradient(135deg, #e3e3e3 0%, #5d6874 100%)';
             myTitle.style.color = '#201e1e';
             myAuthor.style.color = '#201e1e';
@@ -256,66 +206,62 @@ function createTile(read) {
         myTileFront.appendChild(infoIcon);
         myTileBack.appendChild(mySummary);
         myTileBack.appendChild(collapseInfo);
-        myTile.appendChild(myTileFront)
-        myTile.appendChild(myTileBack)
+        myTile.appendChild(myTileFront);
+        myTile.appendChild(myTileBack);
         myArticle.appendChild(myTile);
-
     });
 }
 
-/* RETREIVES THE INDEX OF THE SPECIFIC TILES WHERE THE BUTTON IS TOGGLED */
-// Changes the state of the myLibrary array
+const filter = document.querySelector('#filter');
+filter.addEventListener('keyup', filterBook);
+
+const readSelect = document.querySelector('#read');
+readSelect.addEventListener('change', sortRead);
+
+const addBook = document.querySelector('#add-book');
+addBook.addEventListener('click', addBookToLibrary);
+
+const bookForm = document.querySelector('.book-form');
+bookForm.addEventListener('mouseover', function (e) {
+    const submitFormButton = document.querySelector('#add-book');
+    const isValid = checkValidation();
+    submitFormButton.disabled = !isValid;
+});
+
 const displayTiles = document.querySelector('.display-tiles');
 displayTiles.addEventListener('change', (e) => {
     const parent = e.target.parentNode.parentNode.parentNode.parentNode.parentNode;
     const child = e.target.parentNode.parentNode.parentNode.parentNode;
     const index = Array.prototype.indexOf.call(parent.children, child);
-    /* It has to be background-image, not background-color, for gradients. */
     const pElements = child.querySelectorAll('p');
     const headElement = child.querySelector('h3');
-    if (e.target.checked) {
-        child.style.backgroundImage = 'linear-gradient(135deg, #3a5271 0%, #8bd8e2 100%)';
-        myLibrary[index].read = true;
-        pElements.forEach((paragraph) => {
-            paragraph.style.color = '#ffffff';
-        })
-        headElement.style.color = '#ffffff'
-        bookCount();
-    } else {
-        child.style.backgroundImage = 'linear-gradient(135deg, #e3e3e3 0%, #5d6874 100%)';
-        myLibrary[index].read = false;
-        pElements.forEach((paragraph) => {
-            paragraph.style.color = '#201e1e';
-        })
-        headElement.style.color = '#201e1e';
-        bookCount();
-    }
+    const isChecked = e.target.checked;
+    child.style.backgroundImage = isChecked ? 'linear-gradient(135deg, #3a5271 0%, #8bd8e2 100%)' : 'linear-gradient(135deg, #e3e3e3 0%, #5d6874 100%)';
+    myLibrary[index].read = isChecked;
+    pElements.forEach((paragraph) => (paragraph.style.color = isChecked ? '#ffffff' : '#201e1e'));
+    headElement.style.color = isChecked ? '#ffffff' : '#201e1e';
+    bookCount();
 });
 
-// Event listener to remove Book from array and tiles
 displayTiles.addEventListener('click', (e) => {
-    /* RETREIVES THE INDEX OF THE SPECIFIC TILES WHERE THE BUTTON IS TOGGLED */
     const parent = e.target.parentNode.parentNode.parentNode;
     const child = e.target.parentNode.parentNode;
     const index = Array.prototype.indexOf.call(parent.children, child);
     if (e.target.className === 'remove-book') {
-        // remove object from the myLibrary array as well as the DOM element
         myLibrary.splice(index, 1);
         child.remove();
         bookCount();
     }
     addRemoveMsg();
-
 });
 
-// Event listener to add Book to array and tiles
 const newBook = document.querySelector('.new-book');
-newBook.addEventListener('click',() => {
+newBook.addEventListener('click', () => {
     const formContainer = document.querySelector('.form-container');
     const form = document.querySelector('.book-form');
     formContainer.style.display = 'flex';
     form.style.display = 'flex';
-})
+});
 
 const closeForm = document.querySelector('.close-form');
 closeForm.addEventListener('click', () => {
@@ -323,27 +269,21 @@ closeForm.addEventListener('click', () => {
     const form = document.querySelector('.book-form');
     formContainer.style.display = 'none';
     form.style.display = 'none';
-})
-
+});
 
 document.addEventListener('click', (e) => {
-    if (e.target.id === "front-flip") {
+    if (e.target.id === 'front-flip') {
         e.target.parentNode.parentNode.parentNode.style.transform = 'rotateY(180deg)';
         e.target.parentNode.parentNode.parentNode.style.transform = 'preserve-3d';
         e.target.parentNode.parentNode.parentNode.style.transition = 'transform 0.8s';
         e.target.parentNode.parentNode.nextElementSibling.style.transform = 'rotateY(180deg)';
-
-        // Set the backside of the card/container to display
         e.target.parentNode.parentNode.nextElementSibling.style.display = 'block';
-        // Set the frontside of the card/container to not display
         e.target.parentNode.parentNode.style.display = 'none';
-    } else if (e.target.id === "back-flip") {
+    } else if (e.target.id === 'back-flip') {
         e.target.parentNode.parentNode.parentNode.style.transform = 'rotateY(360deg)';
         e.target.parentNode.parentNode.parentNode.style.transform = 'preserve-3d';
         e.target.parentNode.parentNode.parentNode.style.transition = 'transform 0.8s';
-        // Set the frontside of the card/container to display
         e.target.parentNode.parentNode.previousElementSibling.style.display = 'block';
-        // Set the backside of the card/container to not display
         e.target.parentNode.parentNode.style.display = 'none';
     }
-})
+});
